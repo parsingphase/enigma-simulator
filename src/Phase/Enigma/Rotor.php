5f86be2bb07c3e10d1fd9433bb0410c2316b83da
Richard George <richard@phase.org>
2014-09-25 15:22:20 +0100

    Verify rotor mappings

--- src/Phase/Enigma/Rotor.php
+++ src/Phase/Enigma/Rotor.php
@@ -9,5 +9,51 @@ namespace Phase\Enigma;
 
 class Rotor implements EncryptorInterface
 {
+    /**
+     * @var string Single letter; Position of the ring relative to the core
+     */
+    protected $ringOffset;
+
+
+    /**
+     * @var array 26-element character-indexed array of input to output
+     */
+    protected $coreMapping;
+
+    /**
+     * @return string
+     */
+    public function getRingOffset()
+    {
+        return $this->ringOffset;
+    }
+
+    /**
+     * @param string $ringOffset
+     */
+    public function setRingOffset($ringOffset)
+    {
+        $this->ringOffset = $ringOffset;
+    }
+
+    /**
+     * @return array
+     */
+    public function getCoreMapping()
+    {
+        return $this->coreMapping;
+    }
+
+    /**
+     * @param array $coreMapping
+     */
+    public function setCoreMapping($coreMapping)
+    {
+        if (count($coreMapping) != 26) {
+            throw new \InvalidArgumentException("Mapping must have 26 elements");
+        }
+
+        $this->coreMapping = $coreMapping;
+    }
 
 }
--- tests/Phase/Enigma/RotorTest.php
+++ tests/Phase/Enigma/RotorTest.php
@@ -11,9 +11,58 @@ namespace Phase\Enigma;
 
 class RotorTest extends \PHPUnit_Framework_TestCase
 {
-    public function testRotorIsAnEncryptor ()
+    public function testRotorIsAnEncryptor()
     {
         $rotor = new Rotor();
         $this->assertTrue($rotor instanceof EncryptorInterface, "Rotor must support EncryptorInterface");
     }
+
+    public function testSetAndRememberRingOffset()
+    {
+        $rotor = new Rotor();
+        $ringOffset = 15;
+        $rotor->setRingOffset($ringOffset);
+        $this->assertSame($ringOffset, $rotor->getRingOffset());
+    }
+
+//    public function testSetAndRememberCoreMapping ()
+//    {
+//        $rotor = new Rotor();
+//        $coreMapping=array();
+//        $rotor->setCoreMapping($coreMapping);
+//        $this->assertSame($coreMapping,$rotor->getCoreMapping());
+//
+//    }
+
+    public function testSetAndRememberValidCoreIdentityMapping ()
+    {
+        $rotor = new Rotor();
+        $coreMapping=array(
+            'A'=>'A','B'=>'B','C'=>'C','D'=>'D','E'=>'E','F'=>'F','G'=>'G',
+            'H'=>'H','I'=>'I','J'=>'J','K'=>'K','L'=>'L','M'=>'M','N'=>'N',
+            'O'=>'O','P'=>'P','Q'=>'Q','R'=>'R','S'=>'S','T'=>'T','U'=>'U',
+            'V'=>'V','W'=>'W','X'=>'X','Y'=>'Y','Z'=>'Z'
+        );
+        $rotor->setCoreMapping($coreMapping);
+        $this->assertSame($coreMapping,$rotor->getCoreMapping());
+    }
+
+    /**
+     * Rotors should refuse to accept invalid arrays
+     *
+     * @expectedException \InvalidArgumentException
+     */
+    public function testSetInvalidCoreMappingThrowsException()
+    {
+        $this->setExpectedException('Exception');
+        $rotor = new Rotor();
+        $coreMapping=array(
+            'A'=>'A','B'=>'B','C'=>'C','D'=>'D','E'=>'E','F'=>'F','G'=>'G',
+            'H'=>'H','I'=>'I','J'=>'J','K'=>'K','L'=>'L','M'=>'M','N'=>'N',
+            'O'=>'O','P'=>'P','Q'=>'Q','R'=>'R','S'=>'S','T'=>'T','U'=>'U',
+            'V'=>'V','W'=>'W','X'=>'X','Y'=>'Y'
+        );
+        $rotor->setCoreMapping($coreMapping);
+    }
+
 }
