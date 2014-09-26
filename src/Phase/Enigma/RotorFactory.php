6a178330b9306bc8753d36c5fe84e31e1a35f809
Richard George <richard@phase.org>
2014-09-26 15:37:32 +0100

    Factory and tests for standard rotors

---
+++ src/Phase/Enigma/RotorFactory.php
@@ -0,0 +1,73 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: parsingphase
+ * Date: 26/09/14
+ * Time: 14:35
+ */
+
+namespace Phase\Enigma;
+
+
+class RotorFactory
+{
+
+    const ROTOR_ONE = 1;
+    const ROTOR_TWO = 2;
+    const ROTOR_THREE = 3;
+    const ROTOR_FOUR = 4;
+    const ROTOR_FIVE = 5;
+    const ROTOR_SIX = 6;
+    const ROTOR_SEVEN = 7;
+    const ROTOR_EIGHT = 8;
+    const ROTOR_BETA = 'B';
+    const ROTOR_GAMMA = 'G';
+
+    /**
+     * Simple representation of output lists against inputs A-Z
+     *
+     * @var array
+     */
+    protected $rotorSpecStrings = [
+        self::ROTOR_ONE => 'EKMFLGDQVZNTOWYHXUSPAIBRCJ',
+        self::ROTOR_TWO => 'AJDKSIRUXBLHWTMCQGZNPYFVOE',
+        self::ROTOR_THREE => 'BDFHJLCPRTXVZNYEIWGAKMUSQO',
+        self::ROTOR_FOUR => 'ESOVPZJAYQUIRHXLNFTGKDCMWB',
+        self::ROTOR_FIVE => 'VZBRGITYUPSDNHLXAWMJQOFECK',
+        self::ROTOR_SIX => 'JPGVOUMFYQBENHZRDKASXLICTW',
+        self::ROTOR_SEVEN => 'NZJHGRCXMYSWBOUFAIVLPEKQDT',
+        self::ROTOR_EIGHT => 'FKQHTLXOCBJSPDZRAMEWNIUYGV',
+        self::ROTOR_BETA => 'LEYJVCNIXWPBQMDRTAKZGFUHOS',
+        self::ROTOR_GAMMA => 'FSOKANUERHMBTIYCWLQPZXVGJD'
+    ];
+
+    /**
+     * @param mixed $instanceId A rotor name from the self::ROTOR_* constant list
+     * @return Rotor
+     */
+    public function buildRotorInstance($instanceId)
+    {
+        $rotorSpecString = $this->getRotorSpecString($instanceId);
+        $outputs = preg_split('//', $rotorSpecString, -1, PREG_SPLIT_NO_EMPTY);
+        $inputs = preg_split('//', 'ABCDEFGHIJKLMNOPQRSTUVWXYZ', -1, PREG_SPLIT_NO_EMPTY);
+        $coreMapping = array_combine($inputs, $outputs);
+        $rotor = new Rotor();
+        $rotor->setCoreMapping($coreMapping);
+        $rotor->setRingOffset('A'); // useful default
+        return $rotor;
+    }
+
+    public function getSupportedRotorIdentities()
+    {
+        return array_keys($this->rotorSpecStrings);
+    }
+
+    protected function getRotorSpecString($rotorId)
+    {
+        if (isset($this->rotorSpecStrings[$rotorId])) {
+            return $this->rotorSpecStrings[$rotorId];
+        } else {
+            throw new \InvalidArgumentException;
+        }
+    }
+}
---
+++ tests/Phase/Enigma/RotorFactoryTest.php
@@ -0,0 +1,83 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: parsingphase
+ * Date: 26/09/14
+ * Time: 14:42
+ */
+
+namespace Phase\Enigma;
+
+
+class RotorFactoryTest extends \PHPUnit_Framework_TestCase
+{
+
+    public function testFactoryBuildsRotorsWithValidName()
+    {
+        $factory = new RotorFactory();
+        $this->assertTrue($factory instanceof RotorFactory);
+
+        $rotor = $factory->buildRotorInstance(RotorFactory::ROTOR_ONE);
+        $this->assertTrue($rotor instanceof Rotor);
+    }
+
+    /**
+     * @expectedException \InvalidArgumentException
+     */
+    public function testFactoryRejectsBuildsWithBadName()
+    {
+        $factory = new RotorFactory();
+        $this->assertTrue($factory instanceof RotorFactory);
+
+        $factory->buildRotorInstance('NOSUCHROTOR');
+    }
+
+
+    /**
+     * Make sure each rotor's mapping is coherent
+     * @dataProvider supportedRotorsDataProvider
+     *
+     * @param Rotor $rotor Rotor to test
+     */
+    function testCoherentRotorIdentities(Rotor $rotor)
+    {
+        $mapping = $rotor->getCoreMapping();
+        $this->assertTrue(is_array($mapping));
+        $this->assertSame(26, count($mapping));
+
+        $inputsSeen = array();
+        $outputsSeen = array();
+
+        foreach ($mapping as $i => $o) {
+            $this->assertFalse(isset($inputsSeen[$i]), 'Input seen before');
+            $this->assertFalse(isset($outputsSeen[$o]), 'Output seen before');
+            $this->assertSame(
+                1,
+                preg_match('/^[A-Z]$/', $i),
+                'Input "' . $i . '" must be upper-case letter'
+            );
+            $this->assertSame(
+                1,
+                preg_match('/^[A-Z]$/', $o),
+                'Output must be upper-case letter'
+            );
+            $inputsSeen[$i] = true;
+            $outputsSeen[$o] = true;
+        }
+
+        $this->assertSame(26, count($inputsSeen));
+        $this->assertSame(26, count($outputsSeen));
+    }
+
+    public function supportedRotorsDataProvider()
+    {
+        $factory = new RotorFactory();
+        $rotorIds = $factory->getSupportedRotorIdentities();
+        $parameterLists = [];
+        foreach ($rotorIds as $rotorId) {
+            $parameterLists[] = [$factory->buildRotorInstance($rotorId)];
+        }
+        return $parameterLists;
+    }
+
+}
