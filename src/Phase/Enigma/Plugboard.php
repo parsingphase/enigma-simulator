b04fcf875a820aebb95f1dc606c651c9f07518c5
Richard George <richard@phase.org>
2014-09-27 18:43:09 +0100

    Plugboard tests and implementation

---
+++ src/Phase/Enigma/Plugboard.php
@@ -0,0 +1,59 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: parsingphase
+ * Date: 27/09/14
+ * Time: 18:37
+ */
+
+namespace Phase\Enigma;
+
+
+class Plugboard implements EncryptorInterface
+{
+    protected $connections = [];
+
+    public function clearAllConnections()
+    {
+        $this->connections = [];
+    }
+
+    public function addCableConnection($from, $to)
+    {
+        if (!(preg_match('/^[A-Z]$/', $from) && preg_match('/^[A-Z]$/', $to))) {
+            throw new \InvalidArgumentException("Invalid pair $from-$to");
+        }
+
+        if (isset($this->connections[$from])) {
+            throw new \InvalidArgumentException("Socket $from already used");
+        }
+
+        if (isset($this->connections[$to])) {
+            throw new \InvalidArgumentException("Socket $to already used");
+        }
+
+        $this->connections[$from] = $to;
+        $this->connections[$to] = $from;
+    }
+
+    public function setCableConnections($assocArray)
+    {
+        $this->clearAllConnections();
+
+        foreach ($assocArray as $from => $to) {
+            $this->addCableConnection($from, $to);
+        }
+    }
+
+    public function getOutputCharacterForInputCharacter($inputCharacter)
+    {
+        if (isset($this->connections[$inputCharacter])) {
+            $char = $this->connections[$inputCharacter];
+        } else {
+            $char = $inputCharacter;
+        }
+
+        return $char;
+    }
+
+}
---
+++ tests/Phase/Enigma/PlugBoardTest.php
@@ -0,0 +1,105 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: parsingphase
+ * Date: 27/09/14
+ * Time: 18:27
+ */
+
+namespace Phase\Enigma;
+
+
+class PlugboardTest extends \PHPUnit_Framework_TestCase
+{
+    /**
+     * Test various cable combinations, swapped and non-swapped characters
+     * @dataProvider connectionsDataProvider
+     * @param $connections Array pairs of letters
+     * @param $in String single character
+     * @param $out String single character
+     * @return null
+     */
+    function testSubstitutions($connections, $in, $out)
+    {
+        $plugboard = new Plugboard();
+        $plugboard->setCableConnections($connections);
+
+        $this->assertSame(
+            $out,
+            $plugboard->getOutputCharacterForInputCharacter($in),
+            "Plugboard must substitute pair as specified"
+        );
+        $this->assertSame(
+            $in,
+            $plugboard->getOutputCharacterForInputCharacter($out),
+            "Plugboard substitution must be reciprocal"
+        );
+    }
+
+    function connectionsDataProvider()
+    {
+        $testData = [
+            [[], 'A', 'A'], // Test identity mapping with no cables plugged
+            [   // set cables and an input that will swap
+                [
+                    'D' => 'N',
+                    'G' => 'R',
+                    'I' => 'S',
+                    'K' => 'C',
+                    'Q' => 'X',
+                    'T' => 'M',
+                    'P' => 'V',
+                    'H' => 'Y',
+                    'F' => 'W',
+                    'B' => 'J'
+                ],
+                'K',
+                'C',
+            ],
+            [ // set cables and an input that won't swap
+                [
+                    'D' => 'N',
+                    'G' => 'R',
+                    'I' => 'S',
+                    'K' => 'C',
+                    'Q' => 'X',
+                    'T' => 'M',
+                    'P' => 'V',
+                    'H' => 'Y',
+                    'F' => 'W',
+                    'B' => 'J'
+                ],
+                'E',
+                'E',
+            ],
+            [
+                [
+                    'A' => 'T',
+                    'B' => 'L',
+                    'D' => 'F',
+                    'G' => 'J',
+                    'H' => 'M',
+                    'N' => 'W',
+                ],
+                'H',
+                'M',
+            ],
+            [
+                [
+                    'A' => 'T',
+                    'B' => 'L',
+                    'D' => 'F',
+                    'G' => 'J',
+                    'H' => 'M',
+                    'N' => 'W',
+                ],
+                'C',
+                'C',
+            ]
+        ];
+
+        return ($testData);
+    }
+
+    //TODO add tests for bad inputs
+}
